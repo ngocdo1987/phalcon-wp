3db85410cde67354b2b6c3d698f88c7d4f80c4fe
Ngoc Do <nguyenanhngoc.do@gmail.com>
2016-12-11 13:03:33 -0800

    First commit for PHALCON FW

---
+++ app/controllers/IndexController.php
@@ -0,0 +1,21 @@
+<?php
+
+use Phalcon\Mvc\Controller;
+
+class IndexController extends Controller
+{
+    public function indexAction()
+    {
+        //echo "<h1>Hello!</h1>";
+    }
+
+    public function signupAction()
+    {
+
+    }
+
+    public function registerAction()
+    {
+    	
+    }
+}
---
+++ public/index.php
@@ -0,0 +1,78 @@
+<?php
+// https://docs.phalconphp.com/en/latest/reference/xampp.html
+// Phalcon for PHP 7 TS x86
+use Phalcon\Loader;
+use Phalcon\Mvc\View;
+use Phalcon\Mvc\Application;
+use Phalcon\Di\FactoryDefault;
+use Phalcon\Mvc\Url as UrlProvider;
+use Phalcon\Db\Adapter\Pdo\Mysql as DbAdapter;
+
+
+
+// Register an autoloader
+$loader = new Loader();
+
+$loader->registerDirs(
+    [
+        "../app/controllers/",
+        "../app/models/",
+    ]
+);
+
+$loader->register();
+
+
+
+// Create a DI
+$di = new FactoryDefault();
+
+// Setup the view component
+$di->set(
+    "view",
+    function () {
+        $view = new View();
+
+        $view->setViewsDir("../app/views/");
+
+        return $view;
+    }
+);
+
+// Setup a base URI so that all generated URIs include the "tutorial" folder
+$di->set(
+    "url",
+    function () {
+        $url = new UrlProvider();
+
+        $url->setBaseUri("/FW/PHALCON/");
+
+        return $url;
+    }
+);
+
+// Setup the database service
+$di->set(
+    "db",
+    function () {
+        return new DbAdapter(
+            [
+                "host"     => "localhost",
+                "username" => "root",
+                "password" => "",
+                "dbname"   => "fw_phalcon",
+            ]
+        );
+    }
+);
+
+$application = new Application($di);
+
+try {
+    // Handle the request
+    $response = $application->handle();
+
+    $response->send();
+} catch (\Exception $e) {
+    echo "Exception: ", $e->getMessage();
+}
